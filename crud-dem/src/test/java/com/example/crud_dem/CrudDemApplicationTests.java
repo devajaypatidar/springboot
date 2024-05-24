66653340e9045bd91138b52ecf5d6d1e8aff32ce
Ajay Patidar <ajaypatidar2112@gmail.com>
2024-05-24 22:00:16 +0530

    first commit

---
+++ crud-dem/src/test/java/com/example/crud_dem/CrudDemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_dem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
